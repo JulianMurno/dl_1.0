607a90f811d4621c02f333a04dc9b3a7bfefca17
JulianMurno <julianmurno@iresm.edu.ar>
2025-04-18 19:54:50 -0300

    Allowing Hosts

--- sistema/settings.py
+++ sistema/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default="utWmLh-QbpIzSfAtwR9_fsKJBZ9SA
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dl-1-0.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
